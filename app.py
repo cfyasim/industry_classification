79bb2a7a615a88315553a5192846f0674a4f81a5
cfyasim <asim.khan@contify.com>
2023-07-11 12:49:22 +0530

    industry tagging usib=ng inf1

---
+++ app.py
@@ -0,0 +1,99 @@
+import logging
+import secrets
+import traceback
+from datetime import datetime
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from logging import config as logger_config
+import config
+from constants import CLASSES, INDUSTRY_MAPPING
+from serializers import BertText
+import numpy as np
+import torch
+from functools import lru_cache
+from transformers import AutoTokenizer
+
+
+@lru_cache()
+def get_settings():
+    return config.BertClassifierSettings()
+
+
+settings = get_settings()
+logger_config.dictConfig(settings.LOGGING)
+
+
+@lru_cache()
+def get_bert_classifier():
+    neuron_model = torch.jit.load(f"{settings.INF_INDUSTRY_MODEL_FILE_NAME}/")
+    industry_tokenizer = AutoTokenizer.from_pretrained(
+        f"{settings.TOPIC_MODEL_FILE_NAME}/")
+    return neuron_model, industry_tokenizer
+
+neuron_model, tokenizer = get_bert_classifier()
+app = FastAPI(docs_url=None, redoc_url=None)
+security = HTTPBasic()
+
+logger = logging.getLogger(__name__)
+
+
+def is_authenticated_user(
+        credentials: HTTPBasicCredentials = Depends(security),
+        settings: config.BertClassifierSettings = Depends(get_settings)):
+    correct_username = secrets.compare_digest(
+        credentials.username, settings.BERT_CLASSIFICATION_USERNAME
+    )
+    correct_password = secrets.compare_digest(
+        credentials.password, settings.BERT_CLASSIFICATION_PASSWORD
+    )
+    if not (correct_username and correct_password):
+        logger.info(
+            f"Authentication Failed: Incorrect: {credentials.username},"
+            f" username or Password {credentials.password}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return True
+
+
+@app.post('/predict/industry/')
+async def predict_industry(story: BertText,
+                        auth_status: int = Depends(is_authenticated_user)):
+    """This api is used to tag Industry from Text.
+
+    params: story: BertText
+    Return: Tagged Entities
+    """
+    story_id = ""
+    try:
+        dt = datetime.now()
+        data = story.dict()['story']
+        input_text, story_id = data['story_text'], data['story_id']
+        max_length = 128
+        encoding = tokenizer.encode_plus(
+            input_text,
+            max_length=max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        logits = neuron_model(*encoding)[0][0]
+        sigmoid = torch.nn.Sigmoid()
+        probs = sigmoid(logits.squeeze().cpu())
+        predictions = np.zeros(probs.shape)
+        predictions[np.where(probs >= 0.5)] = 1
+        predicted_labels = [CLASSES[idx] for idx, label in
+                            enumerate(predictions) if label == 1]
+        industry_tags = [INDUSTRY_MAPPING[int(i)] for i in predicted_labels]
+        output_labels = {'predicted_tags': industry_tags, "story_id": story_id}
+        logger.info(
+            f"Bert Classifier: completed prediction  for story_id: {story_id} "
+            f"in {(datetime.now() - dt).total_seconds()} Seconds")
+        return output_labels
+    except Exception as err:
+        logger.error(
+            f"Bert Classifier: Error occurred for story id :{story_id} "
+            f" Error: {err} , Traceback: {traceback.format_exc()}")
